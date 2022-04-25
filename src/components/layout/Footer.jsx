6e421822e684c8102e020ddb70065afb083422d2
sgw8 <vasylmiskiv@gmail.com>
2022-04-25 14:08:16 +0300

    alert context and reducer were added

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,13 +1,15 @@
-import { SiNodedotjs } from 'react-icons/si'
+import { SiReact } from 'react-icons/si'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun } from '@fortawesome/fontawesome-free-solid'
 
 const Footer = () => {
   const footerYear = new Date().getFullYear()
 
   return (
-    <footer className="footer p-10 bg-gray-700
+    <footer className="footer p-6 bg-gray-700
     text-primary-content footer-center">
       <div>
-        <SiNodedotjs className='text-2xl'/>
+      <FontAwesomeIcon icon={faSun} style={{'animation': 'spin 6s infinite linear', 'fontSize': '20px'}} />
         <p>Copyright &copy; {footerYear} All right reserved</p>
       </div>
     </footer>
