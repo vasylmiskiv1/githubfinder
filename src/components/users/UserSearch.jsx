2bf204ffdf27d4f1a42a0d4ca50d3991509a64d7
sgw8 <vasylmiskiv@gmail.com>
2022-04-28 15:54:37 +0300

    were fixed some bugs before deployment to vercel

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -6,7 +6,7 @@ import { searchUsers } from "../../context/github/GithubActions"
 export default function UserSearch() {
   const [text, setText] = useState('')
 
-  const { users, dispatch} = useContext(GithubContext)
+  const { users, dispatch } = useContext(GithubContext)
 
   const { setAlert } = useContext(AlertContext)
 
